60b4459fbb58344e8326c77a662f37f8f9fa96ef
Derek J. Huffman <djhuffman@ucdavis.edu>
2022-10-20 17:47:19 -0400

    Adding docstring to function, renaming function, and providing default for filename (DJH 2022 OCT 20)

--- gradescope_rename_files.py
+++ gradescope_rename_files.py
@@ -10,7 +10,27 @@ Written by Derek J. Huffman (October 2022)
 
 import os
 
-def gather_files_and_names(path_to_data, filename):
+
+def rename_files_from_gradescope(path_to_data, 
+                                 filename="submission_metadata.yml"):
+    """
+    Function for renaming files from gradescope.
+
+    Parameters
+    ----------
+    path_to_data : string
+        A string indicating either the absolute or relative path to the
+        gradescope file..
+    filename : string, optional
+        The name of the gradescope metadata file. The default is 
+        submission_metadata.yml.
+
+    Returns
+    -------
+    None.
+        Does not return anything, but it will rename the files and it will 
+        print the renaming information to the console.
+    """
     starting_dir = os.getcwd()
     os.chdir(path_to_data)
     f = open(filename, 'r')
